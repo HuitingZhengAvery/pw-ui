562e07724a93d406af4bba9b145dc4d2681ba8c6
HuitingZhengAvery <avery.zheng.ht@gmail.com>
2021-03-14 15:42:47 -0700

    feat: initial app setup

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className='landing-header'>
+        <div className="landing-logo" />
+        <div className="landing-title">WELCOME TO AVERY'S UNTITLED</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
